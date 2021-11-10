483f6d4239a1e29582aad780575a4dbba5f7d237
Marcus Ikegami <hikegami.6@gmail.com>
2021-11-10 09:44:33 -0700

    Added enemy object with prototypes that pass all tests

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,54 @@
+const Enemy = require('../lib/Enemy');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion.js');
+
+test('Checks for proper enemy obj creation', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
+
+test("gets enemy's health value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+
+    expect(enemy.isAlive()).toBeFalsy();
+
+});
+
+test("gets enemy's attack value", ()  =>{
+    const enemy = new Enemy('goblin', 'sword');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("subtracts from the enemy's health", () =>{
+    const enemy = new Enemy('goblin', 'sword');
+    const oldHealth = enemy.health;
+
+    enemy.reduceHealth(5);
+
+    expect(enemy.health).toBe(oldHealth - 5);
+
+    enemy.reduceHealth(999);
+
+    expect(enemy.health).toEqual(0);
+});
+
+test("gets a description of the enemy", () =>{
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+})
